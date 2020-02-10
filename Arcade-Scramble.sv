4c446e0fb8d1e78f2625d239c12f5de54b3d10bb
NeuroRulez <neuro_999@hotmail.com>
2020-02-10 14:15:16 +0100

    Add Megadrive DB9 Splitter support

--- Arcade-Scramble.sv
+++ Arcade-Scramble.sv
@@ -95,12 +95,18 @@ module emu
 	// 1 - D-/TX
 	// 2..6 - USR2..USR6
 	// Set USER_OUT to 1 to read from USER_IN.
-	input   [6:0] USER_IN,
-	output  [6:0] USER_OUT
+    output	      USER_MODE,	
+	input   [7:0] USER_IN,
+	output  [7:0] USER_OUT
 );
 
 assign VGA_F1    = 0;
-assign USER_OUT  = '1;
+
+wire   joy_split, joy_mdsel;
+wire   [5:0] joy_in = {USER_IN[6],USER_IN[3],USER_IN[5],USER_IN[7],USER_IN[1],USER_IN[2]};
+assign USER_OUT  = |status[31:30] ? {3'b111,joy_split,3'b111,joy_mdsel} : '1;
+assign USER_MODE = |status[31:30] ;
+
 assign LED_USER  = ioctl_download;
 assign LED_DISK  = 0;
 assign LED_POWER = 0;
@@ -114,6 +120,7 @@ localparam CONF_STR = {
 	"H0O1,Aspect Ratio,Original,Wide;",
 	"H1H0O2,Orientation,Vert,Horz;",
 	"O35,Scandoubler Fx,None,HQ2x,CRT 25%,CRT 50%,CRT 75%;",
+    "OUV,Serial SNAC DB9MD,Off,1 Player,2 Players;",		
 	"-;",
 	"h2O6,Rotation,Buttons,Spinner;",
 	"h2-;",
@@ -177,12 +184,26 @@ wire [15:0] sdram_sz;
 
 wire [10:0] ps2_key;
 
-wire [15:0] joy1,joy2;
+wire [15:0] joy1_USB,joy2_USB;
 wire [15:0] joy = joy1 | joy2;
 
 wire [21:0] gamma_bus;
 
 wire        rom_download = ioctl_download && !ioctl_index;
+
+wire [15:0] joy1 = |status[31:30] ? {joydb9md_1[9],joydb9md_1[10],joydb9md_1[8],joydb9md_1[8:0]} : joy1_USB;
+wire [15:0] joy2 =  status[31]    ? {joydb9md_2[9],joydb9md_2[8],joydb9md_2[10],joydb9md_2[8:0]} : status[30] ? joy1_USB : joy2_USB;
+
+reg [15:0] joydb9md_1,joydb9md_2;
+joy_db9md joy_db9md
+(
+  .clk       ( clk_sys    ), //35-50MHz
+  .joy_split ( joy_split  ),
+  .joy_mdsel ( joy_mdsel  ),
+  .joy_in    ( joy_in     ),
+  .joystick1 ( joydb9md_1 ),
+  .joystick2 ( joydb9md_2 )	  
+);
 
 hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
 (
@@ -205,8 +226,8 @@ hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
 	.ioctl_index(ioctl_index),
 	.sdram_sz(sdram_sz),
 
-	.joystick_0(joy1),
-	.joystick_1(joy2),
+	.joystick_0(joy1_USB),
+	.joystick_1(joy2_USB),
 	.ps2_key(ps2_key)
 );
 
